d863914ae9f1104a177651c9cc87fe732b3dc9a5
Dimitrije <dimitrijes007@gmail.com>
2024-01-03 17:59:02 +0100

    Malo dodajem

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let curDate = new Date();
 let time = curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds();
 console.log(time);
+
 function updateTime(){
     curDate = new Date();
-    time = curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds();
+    let [hours, minutes, seconds] = [curDate.getHours(), curDate.getMinutes(), curDate.getSeconds()];
+    if(hours < 10)
+        hours = '0' + hours;
+    if(minutes < 10)
+        minutes = '0' + minutes;
+    if(seconds < 10)
+        seconds = '0' + seconds;
+    time = hours + ":" + minutes + ":" + seconds;
     document.getElementById("time").innerHTML = time;
 }
 
-setInterval(updateTime, 1000)
+setInterval(updateTime, 1000);
